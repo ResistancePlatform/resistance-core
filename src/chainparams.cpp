7a26111b4959777b01481f92d9081cc69d05a58e
Solar Designer <solar@resistance.io>
2018-11-16 17:40:46 +0100

    Update DNS seeds to resistance.io hostnames

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -148,9 +148,7 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("z.cash", "dnsseed.z.cash")); // Zcash
-        vSeeds.push_back(CDNSSeedData("str4d.xyz", "dnsseed.str4d.xyz")); // @str4d
-        vSeeds.push_back(CDNSSeedData("znodes.org", "dnsseed.znodes.org")); // @bitcartel
+        vSeeds.push_back(CDNSSeedData("resistance.io", "dnsseed.resistance.io"));
 
         // guarantees the first 2 characters, when base58 encoded, are "r1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x1B,0x97};
@@ -269,7 +267,8 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("z.cash", "dnsseed.testnet.z.cash")); // Zcash
+        vSeeds.push_back(CDNSSeedData("testnet.resistance.io", "testnet.resistance.io"));
+        vSeeds.push_back(CDNSSeedData("dnsseed.resistance.io", "dnsseed.resistance.io"));
 
         // guarantees the first 2 characters, when base58 encoded, are "rp"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x1C,0x0C};

d407a62456b9ae3091b19319a7ffef17b67ace70
growlot <growlot.71@gmail.com>
2019-07-25 22:58:14 +0300

    Add Resistance licences such as logo and copyright

--- src/util.cpp
+++ src/util.cpp
@@ -919,9 +919,7 @@ void SetThreadPriority(int nPriority)
 
 std::string PrivacyInfo()
 {
-    return "\n" +
-           FormatParagraph(strprintf(_("In order to ensure you are adequately protecting your privacy when using Resistance, please see <%s>."),
-                                     "https://z.cash/support/security/")) + "\n";
+    return "";
 }
 
 std::string LicenseInfo()
@@ -929,6 +927,7 @@ std::string LicenseInfo()
     return "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2009-%i The Bitcoin Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2015-%i The Zcash Developers"), COPYRIGHT_YEAR)) + "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2018-%i The Resistance Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +
